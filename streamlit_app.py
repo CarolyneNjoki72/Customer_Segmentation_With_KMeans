3ad3fdb92fff4374d2dab73bd2aa8a79f22de92b
CarolyneNjoki72 <161461568+CarolyneNjoki72@users.noreply.github.com>
2025-08-18 17:03:26 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,7 @@ if uploaded_file:
     # Visualization (2D case)
     if len(features) == 2:
         plt.figure(figsize=(6,4))
-        sns.scatterplot(x=features[0], y=features[1], hue="Cluster", data=df, palette="tab10")
+        sns.scatterplot(x=features[1], y=features[0], hue="Cluster", data=df, palette="tab10")
      #  plt.scatter(model.cluster_centers_[:,0], model.cluster_centers_[:,1], 
                     # s=200, c="black", marker="X", label="Centers")
         plt.legend()
