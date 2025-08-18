9d6db0a0c0a44a3957e4b438db5261f44449647b
CarolyneNjoki72 <161461568+CarolyneNjoki72@users.noreply.github.com>
2025-08-18 16:00:03 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,25 @@
 import streamlit as st
+import pandas as pd
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-st.title('🎈 App Name')
+# Theme toggle
+mode = st.sidebar.radio("Choose Theme:", ["Light", "Dark"])
 
-st.write('Hello world!')
+if mode == "Dark":
+    st.markdown(
+        """
+        <style>
+        .stApp {
+            background-color: #0e1117;
+            color: #fafafa;
+        }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+
+st.title("Customer Segmentation Dashboard")
+
+
